639bfb30cb63083a75c1472acdbdfd24c8c2bb37
Pranshu-S <pranshushukla06@gmail.com>
2020-07-21 19:13:39 +0530

    Bot Setup

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/pranshu2/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/pranshu2/mybot_ws/devel;/opt/ros/kinetic".split(';') if "/home/pranshu2/mybot_ws/devel;/opt/ros/kinetic" != "" else []
